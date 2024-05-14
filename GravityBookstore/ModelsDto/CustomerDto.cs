21a33e6119128d172eb2dc69e500e475fa0fd366
Katarzyna <katarzyna.pabian357@gmail.com>
2024-05-14 22:09:34 +0200

    prawie working customer adres post

--- GravityBookstore/ModelsDto/CustomerDto.cs
+++ GravityBookstore/ModelsDto/CustomerDto.cs
@@ -13,4 +13,5 @@ public class CustomerPostDto
     public string First_name { get; set; }
     public string Last_name { get; set; }
     public string Email { get; set; }
+    public AddressPostDto Address { get; set; }
 }
